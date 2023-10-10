dd5da07d6c045bc00078ccd4ca708161f3e18a1f
herpichfr <fabiorafaelh@gmail.com>
2023-10-10 10:19:19 +0100

    Improve script to get weather to update only each 15 min

--- tmux/tmux/scripts/getweather.sh
+++ tmux/tmux/scripts/getweather.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-curl wttr.in/?format="%C+%t+%w"
+get_weather () {
+    WEATHER=$(curl -s wttr.in/?format="%C+%t+%w")
+    echo "$WEATHER" > "$HOME"/.cache/tmux-weather/weather.txt
+}
+
+if [ -f "$HOME"/.cache/tmux-weather/weather.txt ]; then
+    if test "$(find "$HOME"/.cache/tmux-weather/weather.txt -mmin +15)"; then
+        get_weather
+    fi
+else
+    get_weather
+fi
+
+cat "$HOME"/.cache/tmux-weather/weather.txt
