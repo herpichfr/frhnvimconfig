9c9506a06fef7c37e1de3c4c910ea985f5f85b6a
F.R. Herpich <herpich@hydra>
2025-12-29 15:17:31 -0300

    Update latex maker to account for bibliography files

--- scripts/runlatex.sh
+++ scripts/runlatex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pdflatex -interaction=nonstopmode $1 $> /dev/null
-bibtex $1 &> /dev/null
+bibtex ${1%.tex}.aux &> /dev/null
 pdflatex -interaction=nonstopmode $1 &> /dev/null
 pdflatex -interaction=nonstopmode $1 &> /dev/null
